e802aac31a3110c064e5f15e24bd9f1d1ee89e80
MicheallAbaho <michealabaho265@gmail.com>
2017-09-12 12:59:09 +0100

    keyword extraction from tweets by chunking noun phrases and rake API

--- theme_extractions.py
+++ theme_extractions.py
@@ -4,60 +4,64 @@ Created on Sat Jul  8 11:17:13 2017
 
 @author: Mike
 """
+from tweet_processing import working_directory
 from nltk.tokenize import PunktSentenceTokenizer
-from nltk.corpus import state_union
 from textblob import TextBlob
 import pandas as pd
 import numpy as np
 import nltk
 import re
 import io
+import os
 import rake   
 
 class theme_extraction:
     
-    def __init__(self, file, advise_file, non_advise_file, rhemu_corpus):
+    def __init__(self, file, advise_file, non_advise_file, corpus):
         self.file = file
         self.advise_file = advise_file
-        self.rhemu_corpus = rhemu_corpus
-        self.non_advise_file = non_advise_file
+        self.corpus = corpus
+        self.none_advise_file = non_advise_file
     '''Combining all the tweets in their different clusters, advise and non-advise tweets'''
     def combine_tweets(self):
         
         fileConverted = np.array(self.file)
         fileConverted = fileConverted.tolist()
-   
-        for item in fileConverted:
-            unwanted_ctrs = re.compile("[',]")
-            text = item[2].strip('[]')
-            text = unwanted_ctrs.sub('', text)
-            text = text.replace('\\\\', '')
-            self.rhemu_corpus.write(text)
-            self.rhemu_corpus.write('.')
-            self.rhemu_corpus.write(' ')
-            if((item[1] == 'purely_advise') or (item[1] == 'mostly_advise') or (item[1] == 'moderately_advise')):
-                self.advise_file.write(text) 
-                self.advise_file.write('.')
-                self.advise_file.write(' ')
-               
-            else:
-                self.non_advise_file.write(text)
-                self.non_advise_file.write('.')
-                self.non_advise_file.write(' ')
-                
-        self.rhemu_corpus.close()
+        try:
+            for item in fileConverted:
+                unwanted_ctrs = re.compile("[',]")
+                text = item[2].strip('[]')
+                text = unwanted_ctrs.sub('', text)
+                text = re.sub(r'(\\\\\w*)','', text)
+                self.corpus.write(text)
+                self.corpus.write('.')
+                self.corpus.write(' ')
+                if((item[3] == 'purely_advise') or (item[3] == 'mostly_advise') or (item[3] == 'moderately_advise')):
+                    self.advise_file.write(text) 
+                    self.advise_file.write('.')
+                    self.advise_file.write(' ')
+                   
+                else:
+                    self.none_advise_file.write(text)
+                    self.none_advise_file.write('.')
+                    self.none_advise_file.write(' ')
+        except Exception as e:
+            print (str(e))
+            
+        self.corpus.close()
         self.advise_file.close()
-        self.non_advise_file.close()
+        self.none_advise_file.close()
     
 def themes(): 
+    
     #using the Rake module to extract key themes     
     stoppath = "SmartStoplist.txt"
     
-    keythemes_file = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\key_themes.txt', 'a+')
+    keythemes_file = open('diabetes\\diabetes_key_themes.txt', 'a+')
     rake_object = rake.Rake(stoppath, 4,4,4)    # A key theme has to have a minimum of 4 characters, It can be 4 separate phrases, and it has to appear atleast 4 times in the entire corpus
-    advise_file = io.open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\advise.txt', 'r', encoding='iso-8859-1')
-    none_advise_file = io.open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\none_advise.txt', 'r', encoding='iso-8859-1')
-    kph = io.open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\key-rheumatoid-noun-phrases.txt', 'r', encoding='iso-8859-1')
+    advise_file = io.open('diabetes\\advise.txt', 'r', encoding='iso-8859-1')
+    none_advise_file = io.open('diabetes\\none_advise.txt', 'r', encoding='iso-8859-1')
+    kph = io.open('diabetes\\key-rheumatoid-noun-phrases.txt', 'r', encoding='iso-8859-1')
     
     advise_text = advise_file.read()
     none_advise_text = none_advise_file.read()
@@ -91,22 +95,24 @@ def entities():
            word = nltk.word_tokenize(i)
            tagged = nltk.pos_tag(word)
            chunkGram = r"""Chunk: {<RB.?>*<NNP>+<NN>?}"""
-           chunkParser = nltk.ne_chunk(tagged)
+           chunkParser = nltk.ne_chunk(chunkGram)
            chunked = chunkParser.parse(tagged)
-           chunkParser.draw()
+           chunked.draw()
            
-           print (tagged)
-           
+           print (tagged)       
     except Exception as e:
         print (str(e))
 
 #Using Textblob to extract noun phrases from the carge corpus containing tweets
 def extract_noun_phrases():
-    diabetes_text = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\rheumatoid-corpus.txt', 'r').read()
+    working_directory()
+    diabetes_text = open('diabetes\\diabetes-corpus.txt', 'r').read()
     blob = TextBlob(diabetes_text)
     
-    key_phrases = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\key-rheumatoid-noun-phrases.txt', 'a+')
+    key_phrases = open('diabetes\\key-diabetes-noun-phrases.txt', 'a+')
     for phrase in blob.noun_phrases:
+        print (phrase)
+        break
         key_phrases.write(phrase)
         key_phrases.write(',') 
         key_phrases.write(' ')
@@ -114,14 +120,12 @@ def extract_noun_phrases():
         
 
                
-#==============================================================================
-# tdf = pd.read_csv('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\rheumatoid-training-set.csv')
-# rheum_corpus = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\rheumatoid-corpus.txt', 'w+')
-# advise_text = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\advise.txt', 'w+')
-# none_advise_text = open('C:\\Users\\Mike\\Documents\\Github\\ABCi_Twitter_Analysis\\rheum\\new_set\\none_advise.txt', 'w+')
-# th = theme_extraction(tdf, advise_text, none_advise_text, rheum_corpus)
-# th.combine_tweets()
-#==============================================================================
-themes()
+tdf = pd.read_csv('diabetes\\diabetes-training-set-text.csv')
+diabetes_corpus = open('diabetes\\diabetes-corpus.txt', 'w+')
+advise_text = open('diabetes\\diabetes_advise.txt', 'w+')
+none_advise_text = open('diabetes\\diabetes_none_advise.txt', 'w+')
+th = theme_extraction(tdf, advise_text, none_advise_text, diabetes_corpus)
+th.combine_tweets()
+#themes()
 #entities()
 #extract_noun_phrases()
